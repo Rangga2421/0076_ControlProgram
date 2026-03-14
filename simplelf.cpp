8eae87115c84aceadf71f4219f8ed8b7095104eb
Rangga2421 <ranggaardisaputra123@gmail.com>
2026-03-14 14:55:23 +0700

    menambahkan pesan ketika nilai dikoreksi

--- simplelf.cpp
+++ simplelf.cpp
@@ -9,7 +9,8 @@ int main(){
 
     if (x==5){
         x = x+1;
+        cout<<"nilai x sudah dikoreksi menjadi : "<<x<<endl;
     }
 
-    cout << "Bilangan yang diberikan = " << x;
+    cout << "Bilangan yang diberikan = " <<x;
 }
